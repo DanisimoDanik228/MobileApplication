073ba682a2318ba39d7fcd764a41ce95d5b0abe8
danil <danilaorlovic28@gmail.com>
2026-03-30 17:43:17 +0300

    fix: update url

--- app/src/main/java/com/example/mobileapplication/core/Constants.kt
+++ app/src/main/java/com/example/mobileapplication/core/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.mobileapplication.core
 
 object Constants {
-    const val CLOUDFLARETUNNEL_URL = "https://basename-eligible-jobs-steps.trycloudflare.com/"
+    const val CLOUDFLARETUNNEL_URL = "https://lenders-living-shopping-oils.trycloudflare.com/"
     const val NETWORK_CHECK_FREQUENCY = 5000L
     const val NETWORK_TIMEOUT = 3000L
     const val WEATHER_API_KEY = "6925a79742481629ec6edfe0fcdc72b4"
